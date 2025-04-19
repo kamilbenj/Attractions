4d7451c9b4b019742cb9ab5055f247a238c46819
Meder <mederic.rolland@edu.ece.fr>
2025-04-19 13:25:29 +0200

    lien avec le main

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
+
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
